a894c73b3a32c3297e0acc1f8d45e3672bbfc0d9
Luciano Marani <lmarani@santafe.gov.ar>
2023-02-15 09:41:50 -0300

    Ex2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,9 +3,14 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   function includesName(persons, name) {
@@ -18,9 +23,10 @@ const App = () => {
       alert(newName + " is already added to phonebook");
       return;
     } else {
-      const personObject = { name: newName };
+      const personObject = { name: newName, number: newNumber };
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -32,12 +38,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
